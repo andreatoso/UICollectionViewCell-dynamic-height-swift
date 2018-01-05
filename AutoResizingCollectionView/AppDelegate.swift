f78eecc72f80bfcf786b2125495820f2b4942a9e
Andrea Toso <andrea.toso99@gmail.com>
2018-01-05 17:51:54 +0100

    Code added.

--- AutoResizingCollectionView/AppDelegate.swift
+++ AutoResizingCollectionView/AppDelegate.swift
@@ -16,7 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = ViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        
         return true
     }
 
--- AutoResizingCollectionView/ViewController.swift
+++ AutoResizingCollectionView/ViewController.swift
@@ -8,18 +8,108 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+    
+    var layout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        let width = UIScreen.main.bounds.size.width
+        layout.estimatedItemSize = CGSize(width: width, height: 10)
+        return layout
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView?.register(MyCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.collectionViewLayout = layout
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! MyCell
+        
+        if indexPath.item == 0 {
+            cell.label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        }
+        
+        if indexPath.item == 1 {
+            cell.label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+        
+        if indexPath.item == 2 {
+            cell.label.text = "Lorem ipsum dolor."
+        }
+        
+        return cell
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        layout.estimatedItemSize = CGSize(width: view.bounds.size.width, height: 10)
+        super.traitCollectionDidChange(previousTraitCollection)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        layout.estimatedItemSize = CGSize(width: view.bounds.size.width, height: 10)
+        layout.invalidateLayout()
+        super.viewWillTransition(to: size, with: coordinator)
+    }
 }
 
+class MyCell: UICollectionViewCell {
+    
+    lazy var width: NSLayoutConstraint = {
+        let width = contentView.widthAnchor.constraint(equalToConstant: bounds.size.width)
+        width.isActive = true
+        return width
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = UIColor.red
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func systemLayoutSizeFitting(_ targetSize: CGSize, withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority, verticalFittingPriority: UILayoutPriority) -> CGSize {
+        width.constant = bounds.size.width
+        return contentView.systemLayoutSizeFitting(CGSize(width: targetSize.width, height: 1))
+    }
+    
+    fileprivate func setupViews() {
+        contentView.addSubview(label)
+        label.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        label.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        label.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
+        
+        contentView.addSubview(customView)
+        customView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 10).isActive = true
+        customView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        customView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        customView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        contentView.bottomAnchor.constraint(equalTo: customView.bottomAnchor, constant: 10).isActive = true
+    }
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .yellow
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let customView: UIView = {
+        let customView = UIView()
+        customView.backgroundColor = .green
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        return customView
+    }()
+    
+}
